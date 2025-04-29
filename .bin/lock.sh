e838215bddf319fe2fedaebc3aad5bfff5c24956
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-04-28 17:02:07 -0700

    chore(awesome): change i3lock styles;

--- .bin/lock.sh
+++ .bin/lock.sh
@@ -1,36 +1,51 @@
 #!/bin/sh
 
-BLANK='#00000000'
-CLEAR='#ffffff22'
-DEFAULT='#ff00ffcc'
-TEXT='#ee00eeee'
-WRONG='#880000bb'
-VERIFYING='#bb00bbbb'
+PRIMARY="#dc2626ff"
+SECONDARY="#b91c1cff"
+TERTIARY="#450a0aff"
 
 i3lock \
---insidever-color=$CLEAR     \
---ringver-color=$VERIFYING   \
-\
---insidewrong-color=$CLEAR   \
---ringwrong-color=$WRONG     \
-\
---inside-color=$BLANK        \
---ring-color=$DEFAULT        \
---line-color=$BLANK          \
---separator-color=$DEFAULT   \
-\
---verif-color=$TEXT          \
---wrong-color=$TEXT          \
---time-color=$TEXT           \
---date-color=$TEXT           \
---layout-color=$TEXT         \
---keyhl-color=$WRONG         \
---bshl-color=$WRONG          \
-\
---screen 1                   \
---blur 5                     \
---clock                      \
---indicator                  \
---time-str="%H:%M:%S"        \
---date-str="%A, %Y-%m-%d"       \
---keylayout 1                \
+    --blur 5 \
+    --inside-color=$TERTIARY \
+    --insidever-color=$TERTIARY \
+    --insidewrong-color=$TERTIARY \
+    --ring-color=$TERTIARY \
+    --ringver-color=$SECONDARY \
+    --ringwrong-color=$SECONDARY \
+    --separator-color=$TERTIARY \
+    --line-color=$PRIMARY \
+    --keyhl-color=$PRIMARY \
+    --bshl-color=$SECONDARY \
+    --ring-width=4 \
+    --radius=32 \
+    --time-color=A89984FF \
+    --time-pos='ix-180:iy+12' \
+    --time-pos='ix-r-50:iy+12' \
+    --time-str='%H:%M:%S' \
+    --time-font='monospace' \
+    --time-align=2 \
+    --time-size=32 \
+    --date-color=A89984FF \
+    --date-pos='ix+180:iy+12' \
+    --date-pos='ix+r+50:iy+12' \
+    --date-str='%d.%m.%y' \
+    --date-font='monospace' \
+    --date-align=1 \
+    --date-size=32 \
+    --greeter-pos='x+100:iy+12' \
+    --verif-color=00000000 \
+    --wrong-color=00000000 \
+    --modif-color=00000000 \
+    --layout-color=00000000 \
+    --greeter-color=00000000 \
+    --verif-text='' \
+    --wrong-text='' \
+    --noinput-text='' \
+    --lock-text='' \
+    --lockfailed-text='' \
+    --greeter-text='' \
+    --ignore-empty-password \
+    --pass-media-keys \
+    --pass-screen-keys \
+    --indicator \
+    --clock
