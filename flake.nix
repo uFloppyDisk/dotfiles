c3973c150c8436538efc90d385a8872cf18c3552
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-07-17 13:47:37 -0700

    feat(nix): install zen-browser;

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
     home-manager = {
       url = "github:nix-community/home-manager";
