8ef388dcadcae90018ac7b832d2b7b0f06b286f6
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-07-17 17:09:50 -0700

    chore(nix): set zsh as default shell;

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; import ./packages.nix { inherit pkgs; };
+  environment.shells = with pkgs; [ zsh ];
+
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
