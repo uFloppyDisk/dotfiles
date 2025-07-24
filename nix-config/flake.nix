a88f7b2564c168f7cfd21b7d67e9fdacdaca8426
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-07-24 01:59:17 -0700

    chore(nix): ensure zen follows nixpkgs;

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
