4e2fcf8e49bf725a3020d9610d4c197c7bc99372
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-09-12 18:35:55 -0700

    feat(nvim): replace MarkdownPreview with LivePreview;

--- .config/nvim/lua/fd/packer.lua
+++ .config/nvim/lua/fd/packer.lua
@@ -1,97 +1,103 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use 'christoomey/vim-tmux-navigator'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use("christoomey/vim-tmux-navigator")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-    use {
-        'williamboman/mason.nvim',
-        config = function()
-            require("mason").setup({
-                registries = {
-                    "github:mason-org/mason-registry",
-                    "github:Crashdummyy/mason-registry",
-                }
-            })
-        end
-    }
+	use({
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				registries = {
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry",
+				},
+			})
+		end,
+	})
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            --- Uncomment these if you want to manage LSP servers from neovim
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		requires = {
+			--- Uncomment these if you want to manage LSP servers from neovim
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        }
-    }
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	})
 
-    use 'theprimeagen/harpoon'
-    use 'mbbill/undotree'
-    use 'tpope/vim-fugitive'
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
 
-    use({ "stevearc/conform.nvim" })
+	use({ "stevearc/conform.nvim" })
 
-    use {
-        'folke/trouble.nvim',
-        cmd = "Trouble",
-        config = function()
-            require("trouble").setup({})
-        end
-    }
+	use({
+		"folke/trouble.nvim",
+		cmd = "Trouble",
+		config = function()
+			require("trouble").setup({})
+		end,
+	})
 
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
+	use({
+		"brianhuster/live-preview.nvim",
+		requires = {
+			{ "nvim-telescope/telescope.nvim" },
+			{ "ibhagwan/fzf-lua" },
+			{ "echasnovski/mini.pick" },
+			{ "folke/snacks.nvim" },
+		},
+	})
 
-    use {
-        'GustavEikaas/easy-dotnet.nvim',
-        requires = {
-            { 'nvim-lua/plenary.nvim' },
-            { 'nvim-telescope/telescope.nvim' },
-        },
-        config = function()
-            require("easy-dotnet").setup({})
-        end
-    }
+	use({
+		"GustavEikaas/easy-dotnet.nvim",
+		requires = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+		config = function()
+			require("easy-dotnet").setup({})
+		end,
+	})
 
-    use {
-        "seblyng/roslyn.nvim",
-        config = function()
-            require("roslyn").setup({})
-        end
-    }
+	use({
+		"seblyng/roslyn.nvim",
+		config = function()
+			require("roslyn").setup({})
+		end,
+	})
 
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
 end)
